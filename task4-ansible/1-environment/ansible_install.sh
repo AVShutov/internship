37aff28ebc083a35e79e8b5878200388e7900a15
Aliaksei SHutau <ashutau@exadel.com>
2021-06-21 21:04:18 +0300

    Modify docker install

--- task4-ansible/1-environment/ansible_install.sh
+++ task4-ansible/1-environment/ansible_install.sh
@@ -5,16 +5,13 @@ yum install curl git
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python3 get-pip.py --user
 
-python3 -m pip install --user ansible paramiko docker 'docker-compose>= 1.7.0' requests boto3 botocore
-#python3 -m pip install --user paramiko
-#python3 -m pip install --user docker
-#python3 -m pip install --user docker-compose
-#python3 -m pip install --user requests
-#boto3
-#botocore
+python3 -m pip install --user ansible paramiko docker-compose boto3 botocore
 
 ansible-galaxy collection install amazon.aws
+ansible-galaxy collection install community.docker
 
 # Install docker compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+#sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+#sudo chmod +x /usr/local/bin/docker-compose
+
+git clone https://github.com/AVShutov/internship
