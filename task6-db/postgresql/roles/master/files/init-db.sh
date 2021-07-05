afab5adc54c7be0d11c21ad2d269748e6179d733
EC2 Default User <ec2-user@ip-10-0-11-176.eu-central-1.compute.internal>
2021-07-05 01:01:17 +0000

    add pg role

--- task6-db/postgresql/roles/master/files/init-db.sh
+++ task6-db/postgresql/roles/master/files/init-db.sh
@@ -3,32 +3,27 @@ set -e
 set -x
 
 psql -v ON_ERROR_STOP=1 --username postgres --dbname postgres <<-EOSQL
-    CREATE DATABASE task6_db
-        WITH ENCODING = 'UTF8'
-            TABLESPACE = pg_default
-            LC_COLLATE = 'en_US.utf8'
-            LC_CTYPE = 'en_US.utf8'
-            CONNECTION LIMIT = -1;
-    CREATE TABLE task6_db.Students (
-                id int auto_increment,
-                Student varchar (127) not null,
+    CREATE DATABASE task6_db;
+    \c task6_db;
+    create table if not exists Students(
+                id serial primary key,
                 StudentId int not null,
-                primary key (id));
+                Student varchar (127) not null);
 
-    CREATE TABLE task6_db.Result (
-                id int auto_increment,
-                StudentId int,
+    create table if not exists Result(
+                id serial primary key,
+                StudentId int not null,
                 Task1 varchar (127) not null,
                 Task2 varchar (127) not null,
                 Task3 varchar (127) not null,
                 Task4 varchar (127) not null,
-                Task5 varchar (127) not null,
-                primary key (id));
-
-    CREATE USER replica_user WITH REPLICATION ENCRYPTED PASSWORD "$REPLICA_PASSWORD";
+                Task5 varchar (127) not null);
+    \copy Students(StudentId,Student) FROM '/data/Students.csv' DELIMITER ',' CSV HEADER;
+    \copy Result(StudentId,Task1,Task2,Task3,Task4,Task5) FROM '/data/Result.csv' DELIMITER ',' CSV HEADER;
+    CREATE USER replica_user WITH REPLICATION ENCRYPTED PASSWORD '${REPLICA_PASSWORD}';
     SELECT * FROM pg_create_physical_replication_slot('replication_slot_slave1');
 EOSQL
 
-export PGPASSWORD="$POSTGRES_PASSWORD"
-psql -U postgres -c "\copy Students(Student,StudentId) FROM '/data/Students.csv' DELIMITER ',' CSV HEADER;" task6_db
-psql -U postgres task6 -c "\copy Result(StudentId,Task1,Task2,Task3,Task4,Task5) FROM '/data/Result.csv' DELIMITER ',' CSV HEADER;" task6_db
+#export PGPASSWORD="$POSTGRES_PASSWORD"
+#psql -U postgres -h localhost -c "\copy Students(Student,StudentId) FROM './Students.csv' DELIMITER ',' CSV HEADER;" task6_db
+#psql -U postgres -h localhost -c "\copy Result(StudentId,Task1,Task2,Task3,Task4,Task5) FROM './Result.csv' DELIMITER ',' CSV HEADER;" task6_db
